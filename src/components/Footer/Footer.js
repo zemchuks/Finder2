337663b2fe61cb2337a30eaca127aa4acace8c04
Bruno Austin <brunoaugustine2@gmail.com>
2022-02-11 14:28:36 +0100

    refractor

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <Fragment>
             <footer  className="m-3 py-1 text-center bg-white">
-                GitHub FInder &copy; bruno_austine
+                GitHub FInder &copy; benson_madueke
             </footer>
         </Fragment>
     )
